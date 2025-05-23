9c7b0a57788ccfe6324ada7d6f44ac148ed488f0
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-05-23 14:36:14 +0900

    Add FizzBuzz implementation in fizzbuzz.js

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+function main() {
+  for (let n = 1; n < 21; n++) {
+    fizzbuzz(n);
+  }
+}
+
+function fizzbuzz(number) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 == 0) {
+    console.log("Fizz");
+  } else if (number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(`${number}`);
+  }
+}
+
+main();
