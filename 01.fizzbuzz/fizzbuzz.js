46df3fae3940c7aaaba526e957771e33f8905491
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-05-28 10:29:47 +0900

    fix: Change to use strict equals operator

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 function fizzbuzz(number) {
-  if (number % 3 == 0 && number % 5 == 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
-  } else if (number % 5 == 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(`${number}`);
