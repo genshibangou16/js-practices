253ce158e680bc55de5400a82380f9dd424ae04b
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-09 09:40:25 +0900

    データベース接続を関数に渡すように変更し、可読性と再利用性を向上

--- 03.asynchronous/asynchronous.js
+++ 03.asynchronous/asynchronous.js
@@ -13,9 +13,7 @@ const SQL_STATEMENT_INSERT_WITH_ERROR =
   "INSERT INTO books (title, author) VALUES ('吾輩は猫である', '夏目漱石');";
 const SQL_STATEMENT_SELECT_WITH_ERROR = "SELECT id, title, author FROM books;";
 
-const db = new sqlite3.Database(":memory:");
-
-function run(sql, params = []) {
+function run(sql, db, params = []) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
       if (err) {
@@ -26,7 +24,7 @@ function run(sql, params = []) {
     });
   });
 }
-function get(sql, params = []) {
+function get(sql, db, params = []) {
   return new Promise((resolve, reject) => {
     db.get(sql, params, function (err, row) {
       if (err) {
@@ -38,7 +36,7 @@ function get(sql, params = []) {
   });
 }
 
-async function fbcAsynchronousPracticeCallback() {
+async function fbcAsynchronousPracticeCallback(db) {
   db.run(CREATE_BOOKS_TABLE, [], function () {
     db.run(INSERT_BOOK, [], function () {
       console.log(this.lastID);
@@ -73,57 +71,57 @@ async function fbcAsynchronousPracticeCallback() {
   });
 }
 
-async function fbcAsynchronousPracticePromise() {
-  await run(CREATE_BOOKS_TABLE)
-    .then(() => run(INSERT_BOOK))
+async function fbcAsynchronousPracticePromise(db) {
+  await run(CREATE, db)
+    .then(() => run(INSERT, db))
     .then((result) => {
       console.log(result.lastID);
-      return get(SELECT_ALL_BOOKS);
+      return get(SELECT, db);
     })
     .then((result) => {
       console.log(result);
-      return run(DROP_BOOKS_TABLE);
+      return run(DROP, db);
     });
 
   await timers.setTimeout(100);
 
-  await run(CREATE_BOOKS_TABLE)
-    .then(() => run(INSERT_BOOK_WITH_AUTHOR))
+  await run(CREATE, db)
+    .then(() => run(INSERT_BOOK_WITH_AUTHOR, db))
     .then((result) => {
       console.log(result.lastID);
     })
     .catch((error) => {
       console.error(error.message);
     })
-    .then(() => get(SELECT_ALL_BOOKS_WITH_AUTHOR))
+    .then(() => get(SELECT_ALL_BOOKS_WITH_AUTHOR, db))
     .then((result) => {
       console.log(result);
     })
     .catch((error) => {
       console.error(error.message);
     })
-    .finally(() => run(DROP_BOOKS_TABLE));
+    .finally(() => run(DROP, db));
 }
 
-async function fbcAsynchronousPracticeAsyncAwait() {
-  await run(CREATE_BOOKS_TABLE);
-  const resultInsert = await run(INSERT_BOOK);
+async function fbcAsynchronousPracticeAsyncAwait(db) {
+  await run(CREATE, db);
+  const resultInsert = await run(INSERT, db);
   console.log(resultInsert.lastID);
-  const resultSelect = await get(SELECT_ALL_BOOKS);
+  const resultSelect = await get(SELECT, db);
   console.log(resultSelect);
-  await run(DROP_BOOKS_TABLE);
+  await run(DROP, db);
 
   await timers.setTimeout(100);
 
-  await run(CREATE_BOOKS_TABLE);
+  await run(CREATE, db);
   try {
-    const resultInsertErr = await run(INSERT_BOOK_WITH_AUTHOR);
+    const resultInsertErr = await run(INSERT_BOOK_WITH_AUTHOR, db);
     console.log(resultInsertErr.lastID);
   } catch (error) {
     console.error(error.message);
   }
   try {
-    const resultSelectErr = await get(SELECT_ALL_BOOKS_WITH_AUTHOR);
+    const resultSelectErr = await get(SELECT_ALL_BOOKS_WITH_AUTHOR, db);
     console.log(resultSelectErr);
   } catch (error) {
     if (error.code.startsWith("SQLITE")) {
@@ -132,18 +130,19 @@ async function fbcAsynchronousPracticeAsyncAwait() {
       throw error;
     }
   }
-  await run(DROP_BOOKS_TABLE);
+  await run(DROP, db);
 }
 
 async function main() {
+  const db = new sqlite3.Database(":memory:");
   console.log("== コールバック ==");
-  await fbcAsynchronousPracticeCallback();
+  await fbcAsynchronousPracticeCallback(db);
   await timers.setTimeout(100);
   console.log("== Promise ==");
-  await fbcAsynchronousPracticePromise();
+  await fbcAsynchronousPracticePromise(db);
   await timers.setTimeout(100);
   console.log("== async/await ==");
-  await fbcAsynchronousPracticeAsyncAwait();
+  await fbcAsynchronousPracticeAsyncAwait(db);
 }
 
 main();
